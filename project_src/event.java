61205ab3867dfa1350cfe536b0315943a6ee09f1
HaidukYury <HaidukYury@gmail.com>
2017-12-11 19:20:36 +0300

    update
    added primary functional for almous all usecases

--- project_src/event.java
+++ project_src/event.java
@@ -1,44 +1,66 @@
 package sample;
 
-public class event {
-    Integer date;
-    String text;
-    String author;
-    String event_name;
+import javafx.beans.property.SimpleStringProperty;
 
-    public event() {
+public class event {
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty text;
+    private final SimpleStringProperty author;
+    private final SimpleStringProperty event_name;
+
+    public String getDate() {
+        return date.get();
     }
 
-    public Integer getDate() {
+    public SimpleStringProperty dateProperty() {
         return date;
     }
 
-    public void setDate(Integer date) {
-        this.date = date;
+    public void setDate(String date) {
+        this.date.set(date);
     }
 
     public String getText() {
+        return text.get();
+    }
+
+    public SimpleStringProperty textProperty() {
         return text;
     }
 
     public void setText(String text) {
-        this.text = text;
+        this.text.set(text);
     }
 
     public String getAuthor() {
+        return author.get();
+    }
+
+    public SimpleStringProperty authorProperty() {
         return author;
     }
 
     public void setAuthor(String author) {
-        this.author = author;
+        this.author.set(author);
     }
 
     public String getEvent_name() {
+        return event_name.get();
+    }
+
+    public SimpleStringProperty event_nameProperty() {
         return event_name;
     }
 
     public void setEvent_name(String event_name) {
-        this.event_name = event_name;
+        this.event_name.set(event_name);
+    }
+
+    public event(String eventName, String dateIn, String authorIn, String textIn) {
+        this.event_name=new SimpleStringProperty(eventName);
+        this.date=new SimpleStringProperty(dateIn);
+        this.author=new SimpleStringProperty(authorIn);
+        this.text=new SimpleStringProperty(textIn);
     }
 
 
